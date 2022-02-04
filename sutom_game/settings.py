8d1a3f83a447d8969c0e8ad04c0cd16123e4bcdb
Thomas <t.gubeno@fairvision.fr>
2022-02-04 11:19:11 +0100

    try static

--- sutom_game/settings.py
+++ sutom_game/settings.py
@@ -133,7 +133,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
