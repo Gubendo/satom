0aaad9dac245a037876714603a1251f54a918797
Thomas <t.gubeno@fairvision.fr>
2022-02-22 10:34:49 +0100

    add rules

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -4,4 +4,6 @@ from hello_world import views
 urlpatterns = [
     path('', views.hello_world, name='hello_world'),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("rules/", views.rules, name='rules')
+
 ]
--- hello_world/views.py
+++ hello_world/views.py
@@ -2,13 +2,45 @@ from django.shortcuts import render
 from satom.models import Challenge
 from users.models import Roi
 
+
+def calcul_temps(diff):
+    temps = ""
+    if diff / 3600 >= 1:
+        temps = "+ d'1 heure"
+    elif diff / 60 >= 1:
+        minute = diff // 60
+        sec = diff % 60
+        if minute == 1:
+            minstr = " minute"
+        else:
+            minstr = " minutes"
+
+        if sec == 1:
+            secstr = " seconde"
+        else:
+            secstr = " secondes"
+        temps = str(minute) + minstr + " et " + str(sec) + secstr
+
+    else:
+        temps = str(diff) + " secondes"
+    return temps
+
 def hello_world(request):
     daily_chall = Challenge.objects.all().latest('pk')
     daily_pk = daily_chall.pk
 
     king = Roi.objects.all().latest('pk')
+    time = calcul_temps(king.time)
     context = {
         "daily": daily_pk,
         "roi": king,
+        "time": time,
     }
     return render(request, 'hello_world.html', context)
+
+
+def rules(request):
+    context = {
+        
+    }
+    return render(request, 'rules.html', context)
