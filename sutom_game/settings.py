ab23cee41a79caf40ee3dcfe55f4d1991d5e8d71
Gubendo <37445226+Gubendo@users.noreply.github.com>
2025-12-12 16:14:59 +0100

    Update psycopg

--- sutom_game/settings.py
+++ sutom_game/settings.py
@@ -110,8 +110,12 @@ elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
 if ENV == "production":
     # Production: utilise DATABASE_URL (PostgreSQL)
     DATABASES = {
-        "default": env.db()
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
+}
 else:
     # Local: fallback sur SQLite
     DATABASES = {
