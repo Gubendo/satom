a197d536b4681c79937a2cec43f6749994224639
Gubendo <37445226+Gubendo@users.noreply.github.com>
2026-01-09 15:42:08 +0100

    fix burger menu + add paturage to main menu

--- sutom_game/urls.py
+++ sutom_game/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('', include('hello_world.urls')),
     path('user/', include('users.urls')),
     path('satom/', include('satom.urls')),
-    path("paturage/", include("enclose.urls")),
+    path("paturage/", include("enclose.urls"), name="paturage"),
     path("paturage/api/", include("enclose.api.urls")),
 ]
